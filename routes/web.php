60a954d9a0268d0127365a5ab5bbc8fe01145923
AungDwitya <anakagunggede04@gmail.com>
2024-06-07 10:24:11 +0800

    login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 
-Route::get('/', function () {
+Route::get('/', [LoginController::class, 'showLoginForm'])->name('login');
+
+// Route untuk login
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login']);
+
+// Route untuk register
+Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('register', [RegisterController::class, 'register']);
+
+// Route untuk halaman welcome
+Route::get('/welcome', function () {
     return view('welcome');
-});
+})->name('welcome');
