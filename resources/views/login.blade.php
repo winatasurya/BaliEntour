a68941088363c917fd6c7997004b6af558f20875
WinataSurya <madesuryawinata7@gmail.com>
2024-06-23 14:50:57 +0800

    lg

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -12,13 +12,6 @@
         <div class="form-container sign-up">
             <form action="" method="post">
                 @csrf
-                <h1>Create Account</h1>
-                <span>Use your email for registration</span>
-                <input type="text" name="nama_user" placeholder="Username" required />
-                <input type="email" name="email" placeholder="Email" required />
-                <input type="password" name="password_user" placeholder="Password" required />
-                <input type="text" name="telp_user" placeholder="Phone" required />
-                <input type="text" name="alamat_user" placeholder="Address" required />
                 <button>Sign Up</button>
             </form>
         </div>
@@ -34,11 +27,6 @@
         </div>
         <div class="toggle-container">
             <div class="toggle">
-                <div class="toggle-panel toggle-left">
-                    <h1>Welcome Back!</h1>
-                    <p>To keep connected with us please login with your personal info</p>
-                    <button class="hidden" id="login">Sign In</button>
-                </div>
                 <div class="toggle-panel toggle-right">
                     <h1>Hello, Friend!</h1>
                     <p>Enter your personal details and start journey with us</p>
@@ -47,6 +35,5 @@
             </div>
         </div>
     </div>
-    <script src="{{ asset('js/script.js') }}"></script>
 </body>
 </html>
