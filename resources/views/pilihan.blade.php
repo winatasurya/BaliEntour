8b3952fc0bc4893d90d3dfa75ea9ce00678377ef
AungDwitya <anakagunggede04@gmail.com>
2024-07-01 14:11:05 +0800

    aaa

--- resources/views/pilihan.blade.php
+++ resources/views/pilihan.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Register</title>
   <style>
     body {
       display: flex;
@@ -47,7 +47,7 @@
       color: white;
       cursor: pointer;
     }
-    .col-1 h3, .col-1 p {
+    .col-1 h1, .col-1 p {
       color: white;
     }
     .col-2 {
@@ -58,7 +58,7 @@
       width: 50%;
       border-top-right-radius: 50px;
       border-bottom-right-radius: 50px;
-      background: rgba(0, 0, 0, .5);
+      background: rgba(0, 0, 0, .1);
     }
     .col-2 img {
       width: 160px;
@@ -71,30 +71,24 @@
       color: white;
       cursor: pointer;
     }
-    .col-2 h3, .col-2 p {
+    .col-2 h1, .col-2 p {
       color: white;
     }
   </style>
 </head>
 <body>
-<!-- <h1>Bali<span>EnTour</span></h1> -->
-<div class="container" id="container">
-  <div class="form-container sign-in-container">
-    <form action="{{url('/perusahaan')}}">
-      <h1>Register Your Company!</h1>
-      <p>Register your company to spread some happiness</p>
-      <button class="" id="signUp" > REGISTER NOW</button>
-    </form>
-  </div>
-  <div class="overlay-container">
-    <div class="overlay">
-      <div class="overlay-panel overlay-right">
-        <h1>Register Yourself!</h1>
-        <p>Enter your personal details and start journey with us</p>
-        <a href="{{url('/user')}}">
-          <button class="" id="signUp" >REGISTER NOW</button>
-        </a>
-      </div>
+  <div class="form" id ="pilihan">
+    <div class="col-1">
+      <img src="img/bar.jpg" alt="">
+      <h1>Register Your Account</h1>
+      <p>Register and get more joy here!</p>
+      <a href="{{route('register')}}"><button>Register</button></a>
+    </div>
+    <div class="col-2">
+      <img src="img/spa.jpg" alt="">
+      <h1>Register Your Company</h1>
+      <p>Register your company and grow with us!</p>
+      <a href="{{route('register')}}"><button>Register</button></a>
     </div>
   </div>
 </body>
